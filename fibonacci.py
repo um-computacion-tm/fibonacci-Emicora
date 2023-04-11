aaf8b03093752fab42671fedbc1a8b60961201b9
Coratolo <coratolo@pop-os.localdomain>
2023-04-11 10:41:45 -0300

    first commit

---
+++ fibonacci.py
@@ -0,0 +1,18 @@
+import unittest
+
+def fibonacci(number):
+    fibonacci_numbers = [0,1]
+    total = 1 
+    for i in range(2, number +1):
+        total = fibonacci_numbers[-2] + fibonacci_numbers[-1]
+        fibonacci_numbers.append(total)
+        print("fibonacci_numbers " + str(fibonacci_numbers))
+        print("total " + str(total))
+    return total
+class TestFibonacci(unittest.TestCase):
+    def test(self):
+        self.assertEqual(1, fibonacci(2))
+        self.assertEqual(2, fibonacci(3))
+
+if __name__=="__main__":
+    unittest.main()
